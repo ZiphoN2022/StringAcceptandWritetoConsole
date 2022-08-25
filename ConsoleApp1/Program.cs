02f73f2c54205be0c1027b99e0b0fc24ceac4ff9
CAPITECBANK\CP351225 <ZiphoNdungane@capitecbank.co.za>
2022-08-25 10:47:58 +0200

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string firstName = string.Empty;
+
+
+
+            
+            Console.WriteLine("What is your name");
+
+            firstName = Console.ReadLine();
+
+       
+
+            Console.WriteLine("Welcome to this course " + firstName);
+        }
+    }
+}
